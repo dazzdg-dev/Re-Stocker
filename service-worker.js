c4ae3026d153b2aeaef07bc8c4fa45f4443373a8
dazzdg-dev <dazz.dg@gmail.com>
2025-11-12 14:25:38 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,30 @@
-
-const CACHE = 'restocker-cache-v4';
+const CACHE = 'restocker-cache-v9';
 const ASSETS = [
   './',
   './index.html',
-  './css/styles.css',
-  './js/app.js',
-  './js/db.js',
+  './styles.css',
+  './app.js',
+  './db.js',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (e)=>{
+self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e)=>{
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=> k===CACHE? null : caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e)=>{
+self.addEventListener('fetch', e=>{
+  const req = e.request;
   e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request))
+    caches.match(req).then(cached => cached || fetch(req))
   );
 });
